ffe16d9bcf32557e2e0de36da2579f19dbf84c34
Eric Polerecky <eric@polerecky.com>
2021-07-26 10:06:17 -0400

    wip(rn): setting local crypto to cryto placed on globalThis

--- src/sha.browser.js
+++ src/sha.browser.js
@@ -28,16 +28,20 @@ const digest = async (data, alg) => {
       'Please use a browser with webcrypto support and ensure the code has been delivered securely via HTTPS/TLS and run within a Secure Context'
     )
   }
+
+  // TODO: clean this up, hack to test making this work in React Native + Expo
+  const localDigest = global.globalThis.crypto.subtle.digest ?? crypto.subtle.digest;
+
   switch (alg) {
     case 'sha1':
-      return new Uint8Array(await crypto.subtle.digest({ name: 'SHA-1' }, data))
+      return new Uint8Array(await localDigest({ name: 'SHA-1' }, data))
     case 'sha2-256':
-      return new Uint8Array(await crypto.subtle.digest({ name: 'SHA-256' }, data))
+      return new Uint8Array(await localDigest({ name: 'SHA-256' }, data))
     case 'sha2-512':
-      return new Uint8Array(await crypto.subtle.digest({ name: 'SHA-512' }, data))
+      return new Uint8Array(await localDigest({ name: 'SHA-512' }, data))
     case 'dbl-sha2-256': {
-      const d = await crypto.subtle.digest({ name: 'SHA-256' }, data)
-      return new Uint8Array(await crypto.subtle.digest({ name: 'SHA-256' }, d))
+      const d = await localDigest({ name: 'SHA-256' }, data)
+      return new Uint8Array(await localDigest({ name: 'SHA-256' }, d))
     }
     default:
       throw new Error(`${alg} is not a supported algorithm`)
